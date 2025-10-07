330dc9d49da217b35f42108a91ff6c4b23e892ac
yebin048 <lyna0408@naver.com>
2025-10-07 23:58:45 +0900

    4번 실습

--- main.c
+++ main.c
@@ -3,28 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo (int a, int b)
+int square (int a)
 {
-	return (a+b);
+	return (a*a);
 }
 
-int square (int n)
-{
-	return (n*n);
-}
-
-int get_max (int x, int y)
-{
-	if (x>y)
-		return x;
-	else
-		return y;
-}
 int main(int argc, char *argv[]) {
 	
-	printf("sumTwo result is %i\n", sumTwo (2,3));
-	printf ("square result is  %i\n", square (5));
-	printf ("get_max result is %i\n", get_max (10, 13));
+	int a =2;
+	a= square(a);
+	printf("a=%i\n",a);
 	
-	return 0;
 }

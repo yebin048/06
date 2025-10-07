abda94f22f5c646defa11e027b3ab62092d4cdbf
yebin048 <lyna0408@naver.com>
2025-10-08 00:21:02 +0900

    5번 실습

--- main.c
+++ main.c
@@ -3,15 +3,30 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square (int a)
+int factorial (int n)
 {
-	return (a*a);
+	int res=1;
+	int i;
+	for (i=1; i<=n; i++)
+		res = res *i;
+	return res;
 }
+
+int combination(int n, int r)
+{
+	return factorial (n)/ factorial(n-r)/ factorial (r);
+}
+
+
 
 int main(int argc, char *argv[]) {
 	
-	int a =2;
-	a= square(a);
-	printf("a=%i\n",a);
+	int n,r;
+	int res;
+	printf("Input n and r :");
+	scanf ("%d %d", &n, &r);
 	
+	res=combination(n,r);
+	printf("combination result is :%i\n", res);
+	return 0;
 }
